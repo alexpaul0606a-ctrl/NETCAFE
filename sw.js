4bdb0cbb15aad903afdaffd0da3f7e890ae85aa5
NIPUN ROY <alexpaul0606a@gmail.com>
2026-03-19 23:55:54 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
+const CACHE_NAME = "netcafe-v2";
+
 self.addEventListener("install", (event) => {
-console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/netcafe.png",
+        "/netcafe192.png",
+        ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
